f123b9ad5a064043303b5ca446a8149772947eef
faishal-ai <75568324+faishal-ai@users.noreply.github.com>
2023-10-03 20:24:21 +0700

    Add files via upload

--- Pemilihan2Percobaan109.java
+++ Pemilihan2Percobaan109.java
@@ -1,3 +1,20 @@
+// import java.util.Scanner;
+
+// public class Pemilihan2Percobaan109 {
+//     public static void main(String[] args) {
+//         Scanner input19 = new Scanner(System.in);
+
+//         System.out.print("Masukkan tahun : ");
+//         int tahun = input19.nextInt();
+
+//         if ((tahun % 4) == 0) {
+//             if ((tahun % 100) != 0)
+//                 System.out.print("Tahun Kabisat");
+//         } else         
+//             System.out.print("Bukan Tahun Kabisat");
+//     }
+// }
+
 import java.util.Scanner;
 
 public class Pemilihan2Percobaan109 {
@@ -10,7 +27,10 @@ public class Pemilihan2Percobaan109 {
         if ((tahun % 4) == 0) {
             if ((tahun % 100) != 0)
                 System.out.print("Tahun Kabisat");
+                else 
+                System.out.print("Bukan Tahun Kabisat");
         } else         
             System.out.print("Bukan Tahun Kabisat");
     }
 }
+
